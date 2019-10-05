e639ac61d9f4ea536ec7f2552dc94609c470a5aa
Emilee Mitchell <emcakes@gmail.com>
2019-10-05 13:40:19 -0500

    added mixin

--- src/javascripts/components/studentList/studentList.js
+++ src/javascripts/components/studentList/studentList.js
@@ -1,29 +1,25 @@
 import utilities from '../../helpers/utilities';
 
-
 import studentData from '../../helpers/data/studentData';
 import houseData from '../../helpers/data/houseData';
 
 import studentCard from '../studentCard/studentCard';
 import './studentList.scss';
 
-
+// I need to pass in students i because that's the current student. Which means that student card actually has a student.
+// to figure out what house the studetns are in I need to loop through the houses so I need a double loop (this is my j loop below)
 const createStudentList = () => {
   const students = studentData.getStudents();
   const houses = houseData.getHouses();
   let domString = '<ul class="student-list">';
   for (let i = 0; i < students.length; i += 1) {
     for (let j = 0; j < houses.length; j += 1) {
-      if (students[i].houseID === houses[j].id) {
+      if (students[i].houseId === houses[j].id) {
         domString += studentCard.createStudentCard(students[i], houses[j]);
       }
     }
-    // I need to pass in students i because that's the current student. Which means that student card actually has a student.
-    // to figure out what house the studetns are in I need to loop through the houses so I need a double loop (this is my j loop above)
-    domString += studentCard.createStudentCard(students[i]);
   }
   domString += '</ul>';
-
 
   utilities.printToDom('student-container', domString);
 };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,7 @@
-import 'bootstrap';
-import '../styles/main.scss';
 import navbar from './components/navbar/navbar';
 import studentList from './components/studentList/studentList';
+
+import '../styles/main.scss';
 
 const init = () => {
   navbar.loadNavbar();
